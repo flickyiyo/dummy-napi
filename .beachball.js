7f870fb9df2e148b188cf5406f60194798c3d28a
Edgar Ivan Vazquez Camargo <edgar21_9@hotmail.com>
2023-03-10 11:10:15 -0600

    Added config file

---
+++ .beachball.js
@@ -0,0 +1,8 @@
+/** @typedef {import("beachball/lib/types/BeachballOptions").RepoOptions} RepoOptions */
+
+/** @type {RepoOptions} */
+module.exports = {
+    branch: "master",
+    access: "public",
+  };
+  
